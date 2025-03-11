116a00ed1d13976798b4f8846b832ab1b16e755e
VictoriaCarvalho <vix219@icloud.com>
2025-03-11 14:32:34 -0500

    add lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, item in enumerate(dwarves):
+        print(f"{index + 1}. {item}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + "!" for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(ingredients):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for ingredient in ingredients:
+        if ingredient in cheese_types:
+            return ingredient 
+    return None 
+
+
